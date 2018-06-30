50b09f6557c2d4964dbda93d497c5cfe0e270968
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2018-06-30 12:49:57 +0100

    Added command logging + title change if admin

--- FakePowershell.ps1
+++ FakePowershell.ps1
@@ -2,6 +2,9 @@ function newcmd
 {
 Write-Host "PS $d> " -NoNewline
 $c = Read-Host
+
+if($scamlog){Write-Output $c - $dt >> $scamlogfile}
+
 If ($c  -contains 'ls'){
 ls
 Write-Host $Fine
@@ -26,12 +29,20 @@ if(!$ignoreothercommands){if ($c) {Invoke-Expression $c}}
 newcmd
 }
 $host.ui.RawUI.WindowTitle = "Windows PowerShell"
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+$host.ui.RawUI.WindowTitle = "Administrator: Windows PowerShell"
+}
 #Config start
 $av = "AVG Antivirus"
+$scamlogfile=$env:temp+"\scalog-ps.txt" # %temp%\scalog-ps.txt
+$scamlog=$true # Enable command logging
+$dt=$env:date+$env:time # %date%%time%
 $ignoreothercommands = $false
 $Fine = "Your pc is fine all files are clean - $av"
 $Bad = "Be careful a scammers ip address was seen connected to this machine - $av"
 #Config end
+
 $d = get-location
-Write-Host "Windows PowerShell`nCopyright (C) 2016 Microsoft Corporation. All rights reserved.`n"
+Write-Host "Windows PowerShell`nCopyright (C) Microsoft Corporation. All rights reserved.`n"
 newcmd
